b9eecdf3d0bab992a169f0272ab76db66f6d491e
Eriberto Guzman <eriberto.guzman@outlook.com>
2025-06-04 14:28:40 -0400

    update config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,31 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+-- Get all available colorschemes
+local schemes = vim.fn.getcompletion("", "color")
+
+-- Find the index of the current colorscheme
+local function find_current_index()
+  local current = vim.g.colors_name
+  for i, name in ipairs(schemes) do
+    if name == current then
+      return i
+    end
+  end
+  return 0 -- fallback if not found
+end
+
+-- Create the command to go to the next colorscheme
+vim.api.nvim_create_user_command("NextColorscheme", function()
+  local index = find_current_index()
+  local next_index = (index % #schemes) + 1
+  local next_scheme = schemes[next_index]
+  vim.cmd.colorscheme(next_scheme)
+  print("Switched to colorscheme: " .. next_scheme)
+end, {})
+
+-- Also define a command to just show current scheme
+vim.api.nvim_create_user_command("CurrentColorscheme", function()
+  print("Current colorscheme: " .. (vim.g.colors_name or "none"))
+end, {})
