75ca9b80ddef4343d5bc3175fdc8dbbf8168fb4c
Eriberto Guzman <eriberto.guzman@outlook.com>
2025-07-26 16:35:21 -0400

    add lualine updates

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,61 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  optional = true,
+  opts = function(_, opts)
+    opts.options = {
+      icons_enabled = true,
+      theme = 'powerline_dark',
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+      disabled_filetypes = {
+        statusline = {},
+        winbar = {},
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+      always_show_tabline = true,
+      globalstatus = false,
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        winbar = 1000,
+        refresh_time = 16, -- ~60fps
+        events = {
+          'WinEnter',
+          'BufEnter',
+          'BufWritePost',
+          'SessionLoadPost',
+          'FileChangedShellPost',
+          'VimResized',
+          'Filetype',
+          'CursorMoved',
+          'CursorMovedI',
+          'ModeChanged',
+        },
+      }
+    }
+    opts.sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { 'filename' },
+      lualine_x = { 'encoding', 'fileformat', 'filetype' },
+      lualine_y = { 'progress', 'location' },
+      lualine_z = { 'os.date("%I:%M %p")' }
+    }
+    opts.inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { 'filename' },
+      lualine_x = { 'location' },
+      lualine_y = {},
+      lualine_z = {}
+    }
+    opts.tabline = {}
+    opts.winbar = {}
+    opts.inactive_winbar = {}
+    opts.extensions = {}
+    -- if not vim.g.trouble_lualine then
+    --   table.insert(opts.sections.lualine_c, { "navic", color_correction = "" })
+    -- end
+  end
+}
