cef8b4aac15f3c72c06e6cc89573f3fa8ac34e3f
Eriberto Guzman <eriberto.guzman@outlook.com>
2025-07-28 13:14:46 -0400

    small change

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
+vim.opt.scrolloff = 15
 vim.g.lazyvim_eslint_auto_format = true
 
 -- LSP Server to use for Ruby.
